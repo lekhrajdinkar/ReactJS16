47a19fb5f998bd5ad07a661af712c2689174b6dd
lekhrajdinkar <35016437+lekhrajdinkar@users.noreply.github.com>
2018-09-29 12:28:46 -0700

    formating

--- proj-1/r16/src/App.js
+++ proj-1/r16/src/App.js
@@ -7,8 +7,32 @@ class App1 extends Component {
  // 2. Component state
  state = {
   users: [ { name: 'lekhraj', age: 27 },    { name: 'Manish Bobde', age: 28 } ],
-  reactv: '16'
+  reactv : '16'
 }
+
+// ------ A. Template ------
+//2.JSX, not html. looks like html, get trascompiled into JS
+render() {
+  return (
+    <div className="App">
+    
+      <header className="App-header">
+      <h1 className="App-title">React, version : {this.reactv}  Demo App</h1>
+
+      <button onClick={this.button1_method()}>change react version 1</button>
+      <button onClick={this.button2_method(this,'16_v2')}>change react version 2</button>
+
+      <User> hello - between comp1 tag </User> {/* props.children*/} 
+
+       {/* user 1*/} <User name={this.state.users[0].name} age={this.state.users[0].age} click1={this.event0_method}/>
+       {/* user 2*/} <User name={this.state.users[1].name} age={this.state.users[1].age} />
+   
+      </header>      
+    </div>
+  );
+}
+
+// ---------B. COMPONENT ---------------------
 
 // 3. Pass method ref - when "change react version button" is clicked - App comp PARENT
 button1_method = () => {
@@ -38,26 +62,7 @@ event1_method = (event) => {
   } )
 }
 //1.1 render
-  render() {
-    return (
-      <div className="App">
-      {/* 2. JSX, not html. looks like html, get trascompiled into JS. */}
-        <header className="App-header">
-        
-        <button onClick={this.button1_method()}>change react version</button>
-        <button onClick={this.button2_method(this,'arglekhraj')}>change react version</button>
-
-        <h1 className="App-title">React 16 _ Demo App</h1>
-
-        <User> hello - between comp1 tag </User> {/* props.children*/} 
-
-         {/* user 1*/} <User name={this.state.users[0].name} age={this.state.users[0].age} click1={this.event0_method}/>
-         {/* user 2*/} <User name={this.state.users[1].name} age={this.state.users[1].age} />
-     
-        </header>      
-      </div>
-    );
-  }
+  
 }
 
 export default App1;
